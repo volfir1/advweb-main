6560869aeb5abaf8f03a4fe6b8060dd3e23fc40e
Diablo Volfir <lesteripulansible@gmail.com>
2024-06-26 13:32:05 +0800

    w/login/singup/partial admin

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get("/home",[HomeController::class,"index"]);
+Route::get("/log",[HomeController::class,"log"]);
+
+//Admin Routes
+Route::get('/admin',[AdminController::class,'index']);
+Route::get('/charts',[AdminController::class,'charts']);
+
+//Auth Routes
+Route::get('/login',[AuthController::class,'login']);
+Route::get('/signup',[AuthController::class,'signup']);
